ecc29a6d201a91b5ade23d8af71276207e083aff
Kaique Fernando Monteoliva Ananias <fkaique678@gmail.com>
2023-08-08 21:40:06 -0300

    Commit 08/08

--- 10_vsconnect_React/src/main.tsx
+++ 10_vsconnect_React/src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import './pages/Home'
+// import App from './App'
+// import './index.css'
+// import './pages/Home'
 import Home from './pages/Home'
+// import Servicos from './pages/Servicos'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Home />
+    {/* <Servicos/> */}
   </React.StrictMode>,
 )
--- 10_vsconnect_React/src/pages/Home/index.tsx
+++ 10_vsconnect_React/src/pages/Home/index.tsx
@@ -12,21 +12,41 @@ import Linkedin from  "../../assets/img/linkedin.svg"
 // Todo componete e uma funcao
 function Home() {
 
+    // Mostrar menu no layout responsivo
+let menu = document.getElementById("menu_links") as HTMLCanvasElement; // Obtém o elemento do menu pelo ID
+let sombra: any = document.getElementById("sombra"); // Obtém o elemento da sombra pelo ID
+let menu_barras: any = document.getElementById("menu_barras"); // Obtém o elemento do botão do menu pelo ID
+
+let body = document.getElementsByTagName("body")[0]; // Obtém o elemento body (primeiro elemento)
+
+function mostrarMenu() {
+    if (window.getComputedStyle(menu).left != "10px") { // Verifica se a propriedade 'left' do estilo computado do elemento do menu é diferente de 10px
+        menu.style.left = "10px"; // Define a posição 'left' do menu como 10px movendo ele para direita
+        sombra.style.right = "-10vw"; // Move a sombra para a direita
+        menu_barras.setAttribute("aria-expanded", "true"); // Atualiza o atributo 'aria-expanded' para 'true'
+        menu_barras.setAttribute("aria-label", "fechar menu"); // Atualiza o atributo 'aria-label' para 'fechar menu'
+        body.style.overflow = "hidden"; // Define o overflow do body como "hidden" para evitar a rolagem da página
+    } else {
+        menu.style.left = "-300px"; // Esconde o menu deslocando-o para esquerda
+        sombra.style.right = "110vw"; // Move a sombra para a esquerda para ocultá-la
+        menu_barras.setAttribute("aria-expanded", "false"); // Atualiza o atributo 'aria-expanded' para 'false'
+        menu_barras.setAttribute("aria-label", "abrir menu"); // Atualiza o atributo 'aria-label' para 'abrir menu'
+        body.style.overflow = "auto"; // Restaura o overflow do body para o valor padrão (auto)
+    }
+    menu_barras.classList.toggle("ativo"); // Alterna a classe 'ativo' no botão do menu
+}
+
     return (
         // Tag vazia antes de colocar o codigo html fragmento
         <>
             <div id="sombra"></div>
             <header>
                 <div className="container header_conteudo">
-                    {/* <!-- <a className="menu_barras" href="#"><img onclick="mostrarMenu()" src="../Imagens/Menu de barras.svg" alt=""></a> -->
-                    <!-- Acessibilidade ARIA
-                    aria-label = para indicar o que o elemento faz.
-                    aria-expanded = atributo que salva o status do botão (expandido ou nao - true ou false).
-                    aria-control = especifica qual elemento esta sendo controlado atraves do Id.
-                    aria-haspopup = indica que o elemento tem um popup (true ou false).
-            --> */}
-                    <a className="menu_barras" id="menu_barras" aria-label="abrir menu" aria-expanded="false" aria-controls="menu_links" aria-haspopup="true" /*onClick="mostrarMenu()"*/ href="#"><div className="barras"></div></a>
-                    <img className="header_logo" src={ Logo } alt=""/>
+                    {/* <a 
+                    className="menu_barras" href="#"><img onClick={mostrarMenu} src="../Imagens/Menu de barras.svg" alt=""/></a>  */}
+                 
+                    <a className="menu_barras" id="menu_barras" aria-label="abrir menu" aria-expanded="false" aria-controls="menu_links" aria-haspopup="true" onClick={mostrarMenu} href="#"><div className="barras"></div></a>
+                    <img className="header_logo" src={Logo} alt=""/>
                         <nav>
                             <div id="menu_links" className="menu_links">
                                 <a href="../Home/index.html">home</a>
