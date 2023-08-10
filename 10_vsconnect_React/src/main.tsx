a9dd33b54432e60f73bd30be90c44828e1efa1db
Kaique Fernando Monteoliva Ananias <fkaique678@gmail.com>
2023-08-09 21:52:34 -0300

    09/08

--- 10_vsconnect_React/src/main.tsx
+++ 10_vsconnect_React/src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Footer from './components/Footer'
+import Header from './components/Header'
 // import App from './App'
 // import './index.css'
 // import './pages/Home'
 import Home from './pages/Home'
 // import Servicos from './pages/Servicos'
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header/>
     <Home />
+    <Footer/>
     {/* <Servicos/> */}
   </React.StrictMode>,
 )
--- 10_vsconnect_React/src/pages/Home/index.tsx
+++ 10_vsconnect_React/src/pages/Home/index.tsx
@@ -1,69 +1,21 @@
 // Importar o css
 import "./style.css"
-import Logo from "../../assets/img/logo.svg"
 import Clientes from "../../assets/img/clientes.png"
 import Maos_Dev from "../../assets/img/maos_dev.png"
 import Alfinete from "../../assets/img/alfinete.png"
-import Dev from "../../assets/img/dev.png"
-import Facebook from  "../../assets/img/facebook.svg"
-import Instagram from  "../../assets/img/instagram.svg"
-import Linkedin from  "../../assets/img/linkedin.svg"
+
 
 // Todo componete e uma funcao
 function Home() {
 
-    // Mostrar menu no layout responsivo
-let menu = document.getElementById("menu_links") as HTMLCanvasElement; // Obtém o elemento do menu pelo ID
-let sombra: any = document.getElementById("sombra"); // Obtém o elemento da sombra pelo ID
-let menu_barras: any = document.getElementById("menu_barras"); // Obtém o elemento do botão do menu pelo ID
+ 
 
-let body = document.getElementsByTagName("body")[0]; // Obtém o elemento body (primeiro elemento)
 
-function mostrarMenu() {
-    if (window.getComputedStyle(menu).left != "10px") { // Verifica se a propriedade 'left' do estilo computado do elemento do menu é diferente de 10px
-        menu.style.left = "10px"; // Define a posição 'left' do menu como 10px movendo ele para direita
-        sombra.style.right = "-10vw"; // Move a sombra para a direita
-        menu_barras.setAttribute("aria-expanded", "true"); // Atualiza o atributo 'aria-expanded' para 'true'
-        menu_barras.setAttribute("aria-label", "fechar menu"); // Atualiza o atributo 'aria-label' para 'fechar menu'
-        body.style.overflow = "hidden"; // Define o overflow do body como "hidden" para evitar a rolagem da página
-    } else {
-        menu.style.left = "-300px"; // Esconde o menu deslocando-o para esquerda
-        sombra.style.right = "110vw"; // Move a sombra para a esquerda para ocultá-la
-        menu_barras.setAttribute("aria-expanded", "false"); // Atualiza o atributo 'aria-expanded' para 'false'
-        menu_barras.setAttribute("aria-label", "abrir menu"); // Atualiza o atributo 'aria-label' para 'abrir menu'
-        body.style.overflow = "auto"; // Restaura o overflow do body para o valor padrão (auto)
-    }
-    menu_barras.classList.toggle("ativo"); // Alterna a classe 'ativo' no botão do menu
-}
 
     return (
         // Tag vazia antes de colocar o codigo html fragmento
         <>
-            <div id="sombra"></div>
-            <header>
-                <div className="container header_conteudo">
-                    {/* <a 
-                    className="menu_barras" href="#"><img onClick={mostrarMenu} src="../Imagens/Menu de barras.svg" alt=""/></a>  */}
-                 
-                    <a className="menu_barras" id="menu_barras" aria-label="abrir menu" aria-expanded="false" aria-controls="menu_links" aria-haspopup="true" onClick={mostrarMenu} href="#"><div className="barras"></div></a>
-                    <img className="header_logo" src={Logo} alt=""/>
-                        <nav>
-                            <div id="menu_links" className="menu_links">
-                                <a href="../Home/index.html">home</a>
-                                <a href="#">serviços</a>
-                                <a href="../CadastroDev/index.html">cadastro</a>
-                            </div>
-                            <a className="header_icone_login" href="../Login/index.html">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 512 512">
-                                    <path
-                                        d="M352 96l64 0c17.7 0 32 14.3 32 32l0 256c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0c53 0 96-43 96-96l0-256c0-53-43-96-96-96l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-9.4 182.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L242.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
-                                </svg>
-                            </a>
-                            <a className="header_botao_login" href="../Login/index.html">login</a>
-                        </nav>
-                </div>
-            </header>
+
             <main id="main">
                 <h1>página inicial VSconnect</h1>
                 <section className="banner">
@@ -80,20 +32,20 @@ function mostrarMenu() {
                 </section>
                 <section className="container para_clientes">
                     <div className="span_cliente">
-                        <p>Desde 2015 unindo propósitos.<br/>Uma nova forma de conectar pessoas.</p>
+                        <p>Desde 2015 unindo propósitos.<br />Uma nova forma de conectar pessoas.</p>
                     </div>
                     <div className="clientes_conteudo">
-                        <img src={ Clientes } alt=""/>
-                            <div className="clientes_texto">
-                                <h2>para clientes</h2>
-                                <ul>
-                                    <li>cadastrar serviços</li>
-                                    <li>procurar por desenvolvedores</li>
-                                </ul>
-                                <div>
-                                    <a className="botao clientes_botao_cli" href="#">criar conta</a>
-                                </div>
+                        <img src={Clientes} alt="" />
+                        <div className="clientes_texto">
+                            <h2>para clientes</h2>
+                            <ul>
+                                <li>cadastrar serviços</li>
+                                <li>procurar por desenvolvedores</li>
+                            </ul>
+                            <div>
+                                <a className="botao clientes_botao_cli" href="#">criar conta</a>
                             </div>
+                        </div>
                     </div>
                 </section>
                 <div className="conexao"></div>
@@ -108,64 +60,30 @@ function mostrarMenu() {
                             <a className="botao clientes_botao_devs" href="#">criar conta</a>
                         </div>
                     </div>
-                    <img src={ Maos_Dev } alt=""/>
+                    <img src={Maos_Dev} alt="" />
                 </section>
                 <section className="container servicos">
                     <h2>serviços</h2>
                     <div className="servicos_cards">
                         <div className="card card1">
-                            <img src={ Alfinete } alt=""/>
-                                <h3>Aplicativo para PetShop</h3>
-                                <p>Possuo um Petshop e gostaria de um aplicativo para que meus clientes pudessem agendar serviços
-                                    como: banho e tosa, taxi dog, hotelzinho e etc.</p>
-                                <p>Proposta: A Combinar</p>
+                            <img src={Alfinete} alt="" />
+                            <h3>Aplicativo para PetShop</h3>
+                            <p>Possuo um Petshop e gostaria de um aplicativo para que meus clientes pudessem agendar serviços
+                                como: banho e tosa, taxi dog, hotelzinho e etc.</p>
+                            <p>Proposta: A Combinar</p>
                         </div>
                         <div className="card card2">
-                            <img src={ Alfinete } alt=""/>
-                                <h3>Site para Pizzaria</h3>
-                                <p>Preciso de um site para divulgar o sabores das pizzas, o endereço, os telefones de contato,
-                                    nossas redes sociais.</p>
-                                <p>Proposta: R$750,00</p>
+                            <img src={Alfinete} alt="" />
+                            <h3>Site para Pizzaria</h3>
+                            <p>Preciso de um site para divulgar o sabores das pizzas, o endereço, os telefones de contato,
+                                nossas redes sociais.</p>
+                            <p>Proposta: R$750,00</p>
                         </div>
                     </div>
                     <a href="#">Ver mais serviços</a>
                 </section>
             </main>
-            <footer>
-                <div className="container rodape">
-                    <div className="span_dicas">
-                        <img src={ Dev } alt=""/>
-                            <div className="span_dicas_texto">
-                                <p>Temos algumas dicas para o seu serviço ou freela ser um sucesso, acesse nossa página de
-                                    recomendações para saber mais.</p>
-                                <a className="botao botao_dicas" href="#">mais dicas</a>
-                            </div>
-                    </div>
-                    <div className="rodape_conteudo">
-                        <div className="rodape_conteudo_paginas">
-                            <h2>Páginas</h2>
-                            <ul>
-                                <li>Login</li>
-                                <li>Home</li>
-                                <li>Listar Serviços</li>
-                                <li>Cadastrar Cliente</li>
-                                <li>Cadastrar Desenvolvedor</li>
-                            </ul>
-                        </div>
-                        <img src={ Logo } alt=""/>
-                            <div className="rodape_conteudo_contatos">
-                                <h2>Contatos</h2>
-                                <div>
-                                    <a href="#"><img src={ Facebook }alt=""/></a>
-                                    <a href="#"><img src={ Instagram } alt=""/></a>
-                                    <a href="#"><img src={ Linkedin }alt=""/></a>
-                                </div>
-                                <a href="mailto:">contato@vsconnect.com</a>
-                            </div>
-                    </div>
-                    <p>todos os direitos reservados ©.</p>
-                </div>
-            </footer>
+           
         </>
     )
 }
