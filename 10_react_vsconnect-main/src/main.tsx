9a1bec60bff9f1531b458afc6e8aa3386e78bacb
Kaique Fernando Monteoliva Ananias <fkaique678@gmail.com>
2023-08-18 21:53:42 -0300

    Commit 18/08

--- 10_react_vsconnect-main/src/main.tsx
+++ 10_react_vsconnect-main/src/main.tsx
@@ -5,10 +5,13 @@ import { Route, Routes, BrowserRouter } from "react-router-dom"
 // import App from './App'
 import './index.css'
 import Home from './pages/Home'
+
 import Header from './components/Header'
 import ListaDevs from './pages/ListaDevs'
 import ListaServicos from './pages/ListaServicos'
 import Footer from './components/Footer'
+import PerfilUsuario from './pages/PerfilUsuario'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -18,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/' element={ <Home /> } />
         <Route path='listadevs' element={ <ListaDevs /> } />
         <Route path='listaservicos' element={ <ListaServicos /> } />
+        <Route path='perfil/:idUsuario' element={<PerfilUsuario/>}/>
         
       </Routes>
       <Footer />      
